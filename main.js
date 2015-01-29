8bbcd3374214964e9e1fcba04695600b15759f6a
Jon <jonsobocinski@comcast.net>
2015-01-29 15:53:20 -0800

    fixed asset manager shrunk puma when running across huge landscape

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 var AM = new AssetManager();
 var count = 0;
+var speed = 15;
+
+
 
 
 function Animation(spriteSheet, frameWidth, frameHeight, frameDuration, frames, loop, reverse) {
@@ -37,8 +40,8 @@ Animation.prototype.drawFrame = function (tick, ctx, x, y) {
                  xindex * this.frameWidth, yindex * this.frameHeight,  // source from sheet
                  this.frameWidth, this.frameHeight,
                  x, y,
-                 this.frameWidth,
-                 this.frameHeight);
+                 this.frameWidth*size,//changes size
+                 this.frameHeight*size);
 }
 
 Animation.prototype.currentFrame = function () {
@@ -64,7 +67,7 @@ Puma.prototype.draw = function () {
     if(this.x >(1080 + 512)){
         this.x = (-512) ;
 
-        if(count >=2){
+        if(count >=3){
             count =0;
         }else
             count++;
@@ -72,36 +75,50 @@ Puma.prototype.draw = function () {
 
     if(count === 0){
         var background = new Image();
-        background.src = "./img/background2.jpg";
+        background  = AM.getAsset("./img/background2.jpg")
         this.ctx.drawImage(background,0,0);
         this.y = 475
+        size =1;
+        speed =15;
     }else if(count ===1){
         var background = new Image();
-        background.src = "./img/background.jpg";
+        background= AM.getAsset("./img/background.jpg");
         this.y = 450;
+        size = 1;
+        speed = 15;
         this.ctx.drawImage(background,0,0);
     }else if(count === 2){
         var background = new Image();
-        background.src = "./img/background3.jpg";
-        this.y = 500;
+        background = AM.getAsset("./img/background3.jpg");
+        this.y = 800;
+        size = .15
+        speed =4;
         this.ctx.drawImage(background,0,0);
+    }else if(count === 3){
+        var background = new Image();
+        background= AM.getAsset( "./img/background4.jpg");
+        this.ctx.drawImage(background,0,0);
+        this.y=800;
+        speed = 15;
+        size = 1;
+
     }
 
     this.animation.drawFrame(this.game.clockTick, this.ctx, this.x, this.y);
 }
 
 Puma.prototype.update = function() {
-    this.x += 15;//Move function (left to right)
+    this.x += speed;//Move function (left to right)
 }
 
 
 
 AM.queueDownload("./img/puma.png");
-AM.queueDownload("africa.mp3")
-AM.queueDownload("./img/background.png");
-AM.queueDownload("./img/background2.png");
-AM.queueDownload("./img/background3.png")
-
+AM.queueDownload("africa.mp3");
+AM.queueDownload("./img/background.jpg");
+AM.queueDownload("./img/background2.jpg");
+AM.queueDownload("./img/background3.jpg");
+AM.queueDownload("./img/background4.jpg");
 
 AM.downloadAll(function () {
     var canvas = document.getElementById("gameWorld");
